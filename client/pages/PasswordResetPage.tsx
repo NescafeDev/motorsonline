e7cfa1366e50f6fa23934c7f8d5b7a0568273273
NescafeDev <pshykmaksym@gmail.com>
2025-09-27 02:12:59 +0900

    update password reset page

--- client/pages/PasswordResetPage.tsx
+++ client/pages/PasswordResetPage.tsx
@@ -2,7 +2,7 @@ import PasswordReset from "./PasswordReset";
 
 export default function PasswordResetPage() {
   return (
-    <div className="min-h-screen bg-white flex flex-col">
+    <div className="min-h-screen bg-white flex flex-col w-full">
       <main className="flex-1">
         <PasswordReset />
       </main>
