3b568accce9e415c45b3a3b2e6aab25657d0ad92
NescafeDev <pshykmaksym@gmail.com>
2025-10-17 14:26:18 +0900

    update issues

--- client/pages/UserPage.tsx
+++ client/pages/UserPage.tsx
@@ -4,7 +4,7 @@ import UserForm from "./UserForm";
 export default function UserPage() {
   return (
     <PageContainer>
-      <main className="flex-1 px-6 lg:px-[100px] py-8">
+      <main className="flex-1 px-3 xl:px-[100px] py-8">
         <UserForm />
       </main>
     </PageContainer>
